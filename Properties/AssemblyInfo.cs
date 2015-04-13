88ed22317a7c68fedc42c403f9eec80f3849749d
James Athey <james@apx-labs.com>
2015-04-13 13:15:09 -0400

    Initial import

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿// <copyright company="APX Labs, Inc.">
+//     Copyright (c) APX Labs, Inc. All rights reserved.
+// </copyright>
+using System.Reflection;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("FastAndroidCamera")]
+[assembly: AssemblyDescription("Camera preview callbacks with less overhead on Xamarin.Android.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("APX Labs, Inc.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.0")]
