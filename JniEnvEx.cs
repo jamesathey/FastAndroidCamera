e04e276a7bdb441bc790d13989b24a0f254ee53d
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2016-07-20 09:56:06 -0400

    Fix issues with Xamarin Android 6.1 (Replace reflection calls to JniEnv by the new JniEnvironment static class)

--- JniEnvEx.cs
+++ JniEnvEx.cs
@@ -16,65 +16,32 @@
 //
 // Many thanks to Jonathan Pryor from Xamarin for his assistance
 
-using Android.Runtime;
 using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
+using Java.Interop;
 
 namespace ApxLabs.FastAndroidCamera
 {
 	public enum PrimitiveArrayReleaseMode {
-		CommitAndRelease = 0,
-		Commit = 1,
-		Release = 2
-	}
+        CommitAndRelease = 0,
+        Commit = 1,
+        Release = 2
+    }
 
-	public static class JniEnvEx
-	{
-		delegate IntPtr IntPtr_IntPtr_ref_Boolean_IntPtr (IntPtr env, IntPtr array, ref bool copy);
+    public static class JniEnvEx
+    {
+        public static IntPtr NewByteArray(int length)
+        {
+            return JniEnvironment.Arrays.NewByteArray(length).Handle;
+        }
 
-		delegate void IntPtr_IntPtr_IntPtr_int_void (IntPtr env, IntPtr array, IntPtr elements, int mode);
+        public static unsafe byte* GetByteArrayElements(IntPtr array, bool isCopy)
+        {
+            return (byte*)JniEnvironment.Arrays.GetByteArrayElements(new JniObjectReference(array, JniObjectReferenceType.Global), &isCopy);
+        }
 
-		delegate IntPtr IntPtr_int_IntPtr (IntPtr env, int length);
-
-		static void GetDelegate<TDelegate> (string name, ref TDelegate value)
-			where TDelegate : class
-		{
-			if (value != null)
-				return;
-			var envP = typeof(JNIEnv).GetProperty ("Env", BindingFlags.NonPublic | BindingFlags.Static);
-			var env = envP.GetValue (null);
-			var JniEnvP = env.GetType ().GetField ("JniEnv", BindingFlags.NonPublic | BindingFlags.Instance);
-			var JniEnv = JniEnvP.GetValue (env);
-			var d = JniEnv.GetType ().GetField (name);
-			value = (TDelegate) (object) Marshal.GetDelegateForFunctionPointer ((IntPtr) d.GetValue (JniEnv), typeof (TDelegate));
-		}
-
-		[ThreadStatic]
-		static IntPtr_int_IntPtr _NewByteArray;
-
-		public static IntPtr NewByteArray (int length)
-		{
-			GetDelegate ("NewByteArray", ref _NewByteArray);
-			return _NewByteArray (JNIEnv.Handle, length);
-		}
-
-		[ThreadStatic]
-		static IntPtr_IntPtr_ref_Boolean_IntPtr _GetByteArrayElements;
-
-		public static unsafe byte* GetByteArrayElements (IntPtr array, ref bool isCopy)
-		{
-			GetDelegate ("GetByteArrayElements", ref _GetByteArrayElements);
-			return (byte*) _GetByteArrayElements (JNIEnv.Handle, array, ref isCopy);
-		}
-
-		[ThreadStatic]
-		static IntPtr_IntPtr_IntPtr_int_void _ReleaseByteArrayElements;
-
-		public static unsafe void ReleaseByteArrayElements (IntPtr array, byte* elements, PrimitiveArrayReleaseMode mode)
-		{
-			GetDelegate ("ReleaseByteArrayElements", ref _ReleaseByteArrayElements);
-			_ReleaseByteArrayElements (JNIEnv.Handle, array, (IntPtr)elements, (int)mode);
-		}
-	}
+        public static unsafe void ReleaseByteArrayElements(IntPtr array, byte* elements, PrimitiveArrayReleaseMode mode)
+        {
+            JniEnvironment.Arrays.ReleaseByteArrayElements(new JniObjectReference(array, JniObjectReferenceType.Global), (sbyte*)elements, (JniReleaseArrayElementsMode)mode);
+        }
+    }
 }
